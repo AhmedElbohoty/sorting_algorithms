b7d17d34c42b69ee880bfa5b4cecd6a273fa6803
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-24 04:10:16 +0200

    Add file for quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending
+ *              order using the quick sort algorithm
+ *
+ * @array: pointer to array
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+}
