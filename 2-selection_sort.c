065d100d72eb84bae7f194aaecc7d2098ec23052
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-24 03:36:47 +0200

    Fix issue

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,26 +11,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t min, temp, i, j;
+	size_t min, temp, i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
 
-        if (i != min)
-        {
-            temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            print_array(array, size);
-        }
-    }
+		if (i != min)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
